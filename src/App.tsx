977a6670aeb27a66df4f359cbd9565fb4b4df5b9
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2021-12-26 18:48:18 +0800

    the problem is inside package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import './polyfill'
-
 import { registerRootComponent } from 'expo'
 import { Text } from 'react-native'
-
-if (__DEV__) {
-  import('./flipper-config').then(() => console.log('Flipper Configured'))
-}
 
 export default function App () {
   return <Text>Hello world</Text>
